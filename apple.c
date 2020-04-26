66bd43aa0a49b6eefff63befedad11969aff86b9
Lucas Larson <LucasLarson@riseup.net>
2020-04-26 14:28:56 +0000

    add `argv` array

--- apple.c
+++ apple.c
@@ -2,7 +2,12 @@
 
 int main(int argc, char *argv[], char *envp[], char *apple[]) {
 
-  int i;
+  int i = 0;
+  while (argv[i]) {
+    printf("argv[%d] = %s\n", i, argv[i++]);
+  }
+
+  i = 0;
   while (apple[i]) {
     printf("apple[%d] = %s\n", i, apple[i++]);
   }
