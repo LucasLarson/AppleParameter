47fd06e34b86d89a4169d97eea7c1b00cf8ed007
Lucas Larson <LucasLarson@riseup.net>
2020-04-26 14:10:33 +0000

    initial commit

---
+++ apple.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[], char *envp[], char *apple[]) {
+  int i;
+
+  while(apple[i])
+    printf("apple[%d] = %s\n", i, apple[i++]);
+
+  return 0;
+}
