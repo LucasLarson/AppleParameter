33acec245d97db4207c83b9861de9162a0172349
Lucas Larson <LucasLarson@riseup.net>
2020-04-26 14:32:19 +0000

    add `envp` array

--- apple.c
+++ apple.c
@@ -8,6 +8,11 @@ int main(int argc, char *argv[], char *envp[], char *apple[]) {
   }
 
   i = 0;
+  while (envp[i]) {
+    printf("envp[%d] = %s\n", i, envp[i++]);
+  }
+
+  i = 0;
   while (apple[i]) {
     printf("apple[%d] = %s\n", i, apple[i++]);
   }
