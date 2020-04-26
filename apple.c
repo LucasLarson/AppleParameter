ed0f4571841051e1f551d2491915c0c8b7ec35e0
Lucas Larson <LucasLarson@riseup.net>
2020-04-26 14:15:54 +0000

    add brackets to while statement

--- apple.c
+++ apple.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[], char *envp[], char *apple[]) {
-  int i;
 
-  while(apple[i])
+  int i;
+  while (apple[i]) {
     printf("apple[%d] = %s\n", i, apple[i++]);
+  }
 
   return 0;
 }
