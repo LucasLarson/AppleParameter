b2146d045993f51709a5498beaec0ea2df3c4397
Lucas Larson <LucasLarson@riseup.net>
2020-04-28 02:42:22 +0000

    add header with `int main declaration`

--- apple.c
+++ apple.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "apple.h"
 
 int main(int argc, char *argv[], char *envp[], char *apple[]) {
 
---
+++ apple.h
@@ -0,0 +1,6 @@
+#ifndef APPLEPARAMETER_APPLE_H_
+#define APPLEPARAMETER_APPLE_H_
+
+int main(int, char *[], char *[], char *[]);
+
+#endif  /* APPLEPARAMETER_APPLE_H_ */
