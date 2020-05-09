47e1348cf3c5b9d06c6c3055f75ab7f69cd44df0
Lucas Larson <LucasLarson@riseup.net>
2020-05-08 20:05:17 -0400

    add CMake configuration

---
+++ cmake-build-debug/CMakeFiles/AppleParameter.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "AppleParameter"
+  "AppleParameter.pdb"
+  "CMakeFiles/AppleParameter.dir/AppleParameter/apple.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/AppleParameter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/AppleParameter.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for AppleParameter.
+# This may be replaced when dependencies are built.
